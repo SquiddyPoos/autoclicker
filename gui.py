50bb3996a4f3d5a4beb8f1a7a37ded3266c04649
SquiddyPoos <squid2015guy@gmail.com>
2020-09-10 02:42:53 +0800

    Working on gui, will continue later

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter.font import *
 from autoclicker import Autoclicker, AutoclickError
 
 class App(object):
@@ -6,8 +7,52 @@ class App(object):
     def __init__(self):
         self.root = Tk()
         self.autoclicker = Autoclicker()
+        self.default_colour = self.root["bg"]
+        self.root.geometry("500x600")
         self.setup_gui()
         self.root.mainloop()
 
     def setup_gui(self):
+        title_font = Font(family="TkDefaultFont", size=25)
+        title = Label(self.root, text = "Autoclicker", font = title_font)
+        title.place(relx = 0.5, y = 15, anchor = N)
+        self.km = IntVar()
+        rb1 = Radiobutton(self.root, text = "Mouse", variable = self.km, value = 0, command = self.set_disabled, activebackground = self.default_colour)
+        rb1.place(x = 10, rely = 0.15, anchor = W)
+        rb2 = Radiobutton(self.root, text = "Keyboard", variable = self.km, value = 1, command = self.set_disabled, activebackground = self.default_colour)
+        rb2.place(x = 10, rely = 0.5, anchor = W)
+        frame1 = Frame(self.root, bd = 2, relief = RIDGE)
+        frame1.place(relx = 0.05, rely = 0.15, y = 13, relwidth = 0.9, relheight = 0.3, anchor = NW)
+        self.init_mouse(frame1)
+        frame2 = Frame(self.root, bd = 2, relief = RIDGE)
+        frame2.place(relx = 0.05, rely = 0.5, y = 13, relwidth = 0.9, relheight = 0.3, anchor = NW)
+        self.init_kb(frame2)
+
+    def init_mouse(self, frame):
+        canvas = Canvas(frame)
+        canvas.place(relwidth = 1, relheight = 0.19, anchor = NW)
+        self.root.update()
+        w = canvas.winfo_width()
+        h = canvas.winfo_height()
+        canvas.create_line(0, h, w, h, width = 5)
+        canvas.create_line(w/2, 0, w/2, h, width = 1)
+        self.hold_click = IntVar()
+        hold = Radiobutton(frame, text = "Hold", variable = self.hold_click, value = 0, command = self.swap_frame, activebackground = self.default_colour)
+        hold.place(relx = 0.25, y = 5, anchor = N)
+        click = Radiobutton(frame, text = "Click", variable = self.hold_click, value = 1, command = self.swap_frame, activebackground = self.default_colour)
+        click.place(relx = 0.75, y = 5, anchor = N)
+        self.frame_mouse = Frame(frame)
+        self.frame_mouse.place(relwidth = 1, relheight = 0.8, rely = 0.2)
+
+    def init_kb(self, frame):
         pass
+
+    def set_disabled(self):
+        pass
+
+    def swap_frame(self):
+        pass
+
+
+if __name__ == "__main__":
+    app = App()
