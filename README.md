f5724b007a3190c96167abad5826cebf6fad442b
SquiddyPoos <squid2015guy@gmail.com>
2020-09-10 00:58:33 +0800

    Random commit lol

--- README.md
+++
@@ -1,9 +0,0 @@
-# Autoclicker
-
-A simple autoclicker, written in Python, with the tkinter toolkit. \
-Modules required: keyboard, mouse.
-
-## How to Run
-
-Simply run gui.py, with the required dependencies installed. \
-**Sudo is required in Linux.**
--- gui.py
+++ gui.py
@@ -1,8 +1,13 @@
 from tkinter import *
+from autoclicker import Autoclicker, AutoclickError
 
 class App(object):
 
     def __init__(self):
         self.root = Tk()
+        self.autoclicker = Autoclicker()
         self.setup_gui()
         self.root.mainloop()
+
+    def setup_gui(self):
+        pass
